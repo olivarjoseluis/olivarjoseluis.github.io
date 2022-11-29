9fee336d06217401cff69d3eedf6f6b18b553c17
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2022-11-28 23:12:30 -0500

    Navbar ligth mode ready

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import SwitchModeTheme from './switch-mode-theme/SwitchModeTheme'
+import SwitchModeTheme from './switch-mode-theme/SwitchModeTheme';
+import "./nav.scss";
 
 function Nav() {
   return (
-    <nav>Nav <SwitchModeTheme /></nav>
+    <header>
+      <a href='/' className='navbar-brand' aria-label='Jose Luis Olivar'><span>JL</span></a>
+      <ul className='links-nav'>
+        <li>
+          <a href='#about-me' aria-label='About Jose Luis Olivar'>About me</a>
+        </li>
+        <li>
+          <a href='#work-experience' aria-label='Work experience of Jose Luis Olivar'>Work experience</a>
+        </li>
+        <li>
+          <a href='#education' aria-label='Education of Jose Luis Olivar'>Education</a>
+        </li>
+        <li>
+          <a href='#get-in-touch' aria-label='Get in touch with Jose Luis Olivar'>Get in touch</a>
+        </li>
+        <li><SwitchModeTheme /></li>
+      </ul>
+    </header>
   )
 }
 
