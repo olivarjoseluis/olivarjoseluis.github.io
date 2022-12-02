3837a21f541398de716c772f19e8764bb7986519
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2022-12-02 16:54:32 -0500

    Issue great

--- src/components/about-me/Aboutme.jsx
+++ src/components/about-me/Aboutme.jsx
@@ -13,7 +13,7 @@ const Aboutme = () => {
               <h2>Why should I be part of your team?</h2>
               <p>I’m a <strong>System Engineer</strong> with passion for technology and challenging things. I have 4 years of experience
                 as frontend developer, working as a key player team in different projects. for example, I worked building
-                web sites with <strong>Drupal</strong> and <strong>Wordpress</strong> during the last 2 years. But also I have knowledge working with
+                web sites with <strong>Drupal</strong> and <strong>Wordpress</strong> during the last 2 years. But also I have knowledge working with 
                 <strong>JavaScript</strong>, most of it with <strong>React.js</strong>, I’m willing to participle in any react project so I can share all my
                 experience as a Frontend developer.</p>
             </div>
